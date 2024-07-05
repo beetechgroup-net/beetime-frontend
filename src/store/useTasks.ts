749eab3a9256b1c374561ac9403e717aef68a4ba
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2024-07-05 12:52:12 -0400

    Adicionando Zustand

---
+++ src/store/useTasks.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
+import {Task} from "@/interfaces/Task";
+
+type Store = {
+  tasks: Map<number, Task>
+  setTask: (item: Task) => void
+}
+
+export const useTasksStore = create<Store>()(
+    persist(
+        (setState, getState, store) => ({
+          tasks: new Map(),
+          setTask: (item) => { setState((state) => { state.tasks.set(item.id, item)}) },
+    }),
+{
+  name: "my-tasks-storage",
+      storage: createJSONStorage(() => localStorage),
+},
+),
+)
